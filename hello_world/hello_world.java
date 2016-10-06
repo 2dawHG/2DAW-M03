3609bff8942351a8f8f0fb9d10da46b99b5769da
2dawHG <chg.since1993@gmail.com>
2016-10-06 16:46:10 +0200

    v1.Import hello world

--- hello_world/hello_world.java
+++ hello_world/hello_world.java
@@ -2,6 +2,6 @@ package cat.iesjoaquimmir.myapp.views;
 
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Hello World from Vilanova");
+		System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca)");
 	}
 }
